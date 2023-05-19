c7fe4765c519920a55b60943701691d536b91015
ymatsukawa <ymatsukawa27@gmail.com>
2023-05-19 19:07:39 +0900

    added matrix

--- impl/factor_variance/main.py
+++ impl/factor_variance/main.py
@@ -1,16 +1,18 @@
 from ml.preprocess.load_data import Loader
 from ml.core.model import Model
+from ml.analyze.matrix import Categorize
 
 
-
-
-exit
 
 # preprocess
 loader = Loader("impl/factor_variance/data.csv")
 x_train, x_test, y_train, y_test = loader.load()
 
+cx = Categorize.cat(x_train, y_train)
+
 # fit and predict by model
+"""
 model = Model(x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
 model.fit()
 model.predict(x_test)
+"""
